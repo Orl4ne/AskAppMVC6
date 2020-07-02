6a3267094d137c91004a11fbca83d72a6f00131a
Orlane Vermo <orlane_v@hotmail.com>
2020-07-02 09:12:43 +0200

    adding context and creating database

---
+++ AskAppMVC6/Services/Ask/AskApp.Ask.DAL/AskContext.cs
@@ -0,0 +1,40 @@
+﻿using AskApp.Ask.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskApp.Ask.DAL
+{
+    public class AskContext : DbContext
+    {
+        public AskContext()
+        {
+        }
+        public AskContext(DbContextOptions<AskContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder is null)
+                throw new ArgumentNullException(nameof(optionsBuilder));
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(@"Data Source=askApp.db;");
+                optionsBuilder.EnableSensitiveDataLogging();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (modelBuilder is null)
+                throw new ArgumentNullException(nameof(modelBuilder));
+        }
+
+        public DbSet<QuestionEF> Questions { get; set; }
+        public DbSet<AnswerEF> Answers { get; set; }
+        public DbSet<AskUserEF> AskUsers { get; set; }
+    }
+}
