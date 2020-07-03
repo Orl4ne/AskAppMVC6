cd4d652bddfc3a0af75013baeb0d0ac1f7da1d82
Orlane Vermo <orlane_v@hotmail.com>
2020-07-03 15:51:01 +0200

    Question Repo Get Tests OK

--- AskAppMVC6/Services/Ask/AskApp.Ask.DAL.Tests/QuestionRepoTests/GetQuestionTests.cs
+++ AskAppMVC6/Services/Ask/AskApp.Ask.DAL.Tests/QuestionRepoTests/GetQuestionTests.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AskApp.Ask.DAL.Repositories;
+using AskApp.Common.Interfaces.IRepositories;
+using AskApp.Common.TOs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace AskApp.Ask.DAL.Tests.QuestionRepoTests
@@ -9,93 +15,113 @@ namespace AskApp.Ask.DAL.Tests.QuestionRepoTests
     public class GetQuestionTests
     {
         [TestMethod]
-        public void GetALlAskUsers_Successful()
+        public void GetALlQuestions_Successful()
         {
+            //ARRANGE
             var options = new DbContextOptionsBuilder<AskContext>()
-                .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
-                .Options;
+               .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
+               .Options;
             using var context = new AskContext(options);
             IAskUserRepository askUserRepository = new AskUserRepository(context);
+            IQuestionRepository questionRepository = new QuestionRepository(context);
 
-            //ARRANGE
             var user = new AskUserTO { FirstName = "Jean-Claude", LastName = "DuPet" };
             var user2 = new AskUserTO { FirstName = "Martine", LastName = "ALaPlage" };
-            var user3 = new AskUserTO { FirstName = "Sylvanus", LastName = "Lepélé" };
-            askUserRepository.Create(user);
-            askUserRepository.Create(user2);
-            askUserRepository.Create(user3);
+            var addedUser = askUserRepository.Create(user);
+            var addedUser2 = askUserRepository.Create(user2);
+            context.SaveChanges();
+            DateTime date = DateTime.Now;
+            var question = new QuestionTO { IsArchived = false, Message = "Je n'arrive pas à faire un test!", Title = "Problème avec Tests", Date = date, Author = addedUser };
+            var question2 = new QuestionTO { IsArchived = false, Message = "Comment créer un projet MVC 6?", Title = "MVC6", Date = date, Author = addedUser2 };
+            var question3 = new QuestionTO { IsArchived = false, Message = "Comment faire boucle foreach?", Title = "foreach", Date = date, Author = addedUser2 };
+            var addedQuestion = questionRepository.Create(question);
+            var addedQuestion2 = questionRepository.Create(question2);
+            var addedQuestion3 = questionRepository.Create(question3);
             context.SaveChanges();
 
             //ACT
-            var test = askUserRepository.GetAll();
+            var test = questionRepository.GetAll();
 
             //ASSERT
             Assert.AreEqual(3, test.Count());
         }
 
         [TestMethod]
-        public void GetAllAskUsers_NoAskUserInDb_ThrowException()
+        public void GetAllQuestions_NoQuestionInDb_ThrowException()
         {
             var options = new DbContextOptionsBuilder<AskContext>()
-                .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
-                .Options;
+              .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
+              .Options;
             using var context = new AskContext(options);
             IAskUserRepository askUserRepository = new AskUserRepository(context);
+            IQuestionRepository questionRepository = new QuestionRepository(context);
 
-            Assert.ThrowsException<ArgumentNullException>(() => askUserRepository.GetAll());
+            Assert.ThrowsException<ArgumentNullException>(() => questionRepository.GetAll());
         }
 
         [TestMethod]
-        public void GetAskUserById_Successful()
+        public void GetQuestionById_Successful()
         {
             var options = new DbContextOptionsBuilder<AskContext>()
-                .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
-                .Options;
+               .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
+               .Options;
             using var context = new AskContext(options);
             IAskUserRepository askUserRepository = new AskUserRepository(context);
+            IQuestionRepository questionRepository = new QuestionRepository(context);
             //ARRANGE
             var user = new AskUserTO { FirstName = "Jean-Claude", LastName = "DuPet" };
             var user2 = new AskUserTO { FirstName = "Martine", LastName = "ALaPlage" };
-            var user3 = new AskUserTO { FirstName = "Sylvanus", LastName = "Lepélé" };
-            askUserRepository.Create(user);
-            askUserRepository.Create(user2);
-            askUserRepository.Create(user3);
+            var addedUser = askUserRepository.Create(user);
+            var addedUser2 = askUserRepository.Create(user2);
+            context.SaveChanges();
+            DateTime date = DateTime.Now;
+            var question = new QuestionTO { IsArchived = false, Message = "Je n'arrive pas à faire un test!", Title = "Problème avec Tests", Date = date, Author = addedUser };
+            var question2 = new QuestionTO { IsArchived = false, Message = "Comment créer un projet MVC 6?", Title = "MVC6", Date = date, Author = addedUser2 };
+            var question3 = new QuestionTO { IsArchived = false, Message = "Comment faire boucle foreach?", Title = "foreach", Date = date, Author = addedUser2 };
+            var addedQuestion = questionRepository.Create(question);
+            var addedQuestion2 = questionRepository.Create(question2);
+            var addedQuestion3 = questionRepository.Create(question3);
             context.SaveChanges();
 
             //ACT
-            var test = askUserRepository.GetById(2);
+            var test =questionRepository.GetById(2);
 
             //ASSERT
-            Assert.AreEqual("Martine", test.FirstName);
-
+            Assert.AreEqual("Comment créer un projet MVC 6?", test.Message);
         }
 
         [TestMethod]
-        public void GetAskUserById_ProvidingNoId_ThrowException()
+        public void GetQuestionById_ProvidingNoId_ThrowException()
         {
             var options = new DbContextOptionsBuilder<AskContext>()
                 .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
                 .Options;
             using var context = new AskContext(options);
             IAskUserRepository askUserRepository = new AskUserRepository(context);
+            IQuestionRepository questionRepository = new QuestionRepository(context);
             //ARRANGE
             var user = new AskUserTO { FirstName = "Jean-Claude", LastName = "DuPet" };
+            var addedUser = askUserRepository.Create(user);
+            context.SaveChanges();
+            DateTime date = DateTime.Now;
+            var question = new QuestionTO { IsArchived = false, Message = "Je n'arrive pas à faire un test", Title = "Problème avec Tests", Date = date, Author = addedUser };
 
             //ASSERT
-            Assert.ThrowsException<ArgumentException>(() => askUserRepository.GetById(user.Id));
+            Assert.ThrowsException<ArgumentException>(() => questionRepository.GetById(question.Id));
         }
 
         [TestMethod]
-        public void GetAskUserById_ProvidingNonExistingId_ThrowException()
+        public void GetQuestionById_ProvidingNonExistingId_ThrowException()
         {
             var options = new DbContextOptionsBuilder<AskContext>()
                 .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
                 .Options;
             using var context = new AskContext(options);
             IAskUserRepository askUserRepository = new AskUserRepository(context);
+            IQuestionRepository questionRepository = new QuestionRepository(context);
 
             //ASSERT
-            Assert.ThrowsException<ArgumentNullException>(() => askUserRepository.GetById(14));
+            Assert.ThrowsException<ArgumentNullException>(() => questionRepository.GetById(14));
         }
     }
 }
