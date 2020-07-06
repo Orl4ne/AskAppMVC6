77378ab58dc9e1f58b3e293c79c271424058d2ea
Orlane Vermo <orlane_v@hotmail.com>
2020-07-06 14:29:00 +0200

    ShowThisQuestion UC Tests OK

---
+++ AskAppMVC6/Services/Ask/AskApp.Ask.BLL.Tests/ShowThisQuestionTests.cs
@@ -0,0 +1,36 @@
+﻿using AskApp.Common.Interfaces.IRepositories;
+using AskApp.Common.TOs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskApp.Ask.BLL.Tests
+{
+    [TestClass]
+    public class ShowThisQuestionTests
+    {
+        public QuestionTO MockQuestion()
+        {
+            var user = new AskUserTO { FirstName = "Jean-Claude", LastName = "DuPet" };
+            DateTime date = DateTime.Now;
+            var question = new QuestionTO { IsArchived = false, Message = "Je n'arrive pas à faire un test!", Title = "Problème avec Tests", Date = date, Author = user };
+
+            return question;
+        }
+        [TestMethod]
+        public void ShowThisQuestion_Successful()
+        {
+            var mockQuestionRepository = new Mock<IQuestionRepository>();
+            mockQuestionRepository.Setup(u => u.GetById(It.IsAny<int>()))
+                          .Returns(MockQuestion);
+
+            var askUC = new AskUC(mockQuestionRepository.Object);
+            var questionToShow = askUC.ShowThisQuestion(1);
+            
+            Assert.IsNotNull(questionToShow);
+            mockQuestionRepository.Verify((m => m.GetById(It.IsAny<int>())), Times.Once());
+        }
+    }
+}
